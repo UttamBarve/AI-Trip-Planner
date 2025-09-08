93f1ef506df9ab55353d600c50cf3c0f5d5122f5
Uttam Barve <uttambarve007@gmail.com>
2025-09-09 03:31:42 +0530

    adding responsiveness to the web pages

--- src/components/custom/PlacesToVisit.jsx
+++ src/components/custom/PlacesToVisit.jsx
@@ -12,9 +12,9 @@ export const PlacesToVisit = ({ trip }) => {
               <h2 className="font-medium text-lg">
                 Day {day.day}: {day.theme}
               </h2>
-              <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5 ">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
                 {day.activities?.map((plan, id) => (
-                  <PlaceCards plan={plan} id={id} trip={trip}/>
+                  <PlaceCards plan={plan} id={id} trip={trip} />
                 ))}
               </div>
             </div>
