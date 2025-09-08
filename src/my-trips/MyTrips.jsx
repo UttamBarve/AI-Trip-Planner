5cbf5b015df656e4ac6f5930bbc4a99b0e86cc24
Uttam Barve <uttambarve007@gmail.com>
2025-09-09 00:42:29 +0530

    Finishing Mytrip Functionality

--- src/my-trips/MyTrips.jsx
+++ src/my-trips/MyTrips.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "@/service/firebaseConfig";
-import { IoPricetagsOutline } from "react-icons/io5";
-import { MdPeopleAlt } from "react-icons/md";
-import { LuCalendarDays } from "react-icons/lu";
+import { MyTripCard } from "@/components/custom/MyTripCard";
 
 export const MyTrips = () => {
   useEffect(() => {
@@ -18,53 +16,32 @@ export const MyTrips = () => {
       navigate("/");
       return;
     }
-
     const q = query(
       collection(db, "AITrips"),
       where("userEmail", "==", user?.email)
     );
-    setUserTrips([]);
     const querySnapshot = await getDocs(q);
+    const trips = [];
     querySnapshot.forEach((doc) => {
-      setUserTrips((prevVal) => [...prevVal, doc.data()]);
+      trips.push(doc.data());
     });
+    setUserTrips(trips);
   };
-  
+
   return (
     <div className="sm:px-10 md:px-32 lg:px-56 xl:px-56 px-5 mt-10 flex flex-col ">
       <h2 className="font-bold text-2xl mb-4 border-b-2">My Trip</h2>
       <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5 ">
-        {userTrips.slice().reverse().map((trip, idx) => (
-          <div
-            key={idx}
-            onClick={() => {
-              navigate("/view-trip/" + trip?.id);
-            }}
-            className="border hover:scale-105 rounded-lg hover:shadow-xl  transition-all duration-500 cursor-pointer p-5"
-          >
-            <img
-              className="w-[200px] rounded-xl"
-              src={"/placeholder.jpg"}
-              alt=""
-            />
-
-            <div className="my-3 flex gap-1 flex-col">
-              <h2 className="font-medium text-lg ">{trip?.userSelection?.location}</h2>
-              <h2 className="text-sm text-gray-700 flex gap-1 items-center">
-                <IoPricetagsOutline className="mt-0.5 shrink-0  text-yellow-500" />
-                {trip?.userSelection?.budget}
-              </h2>
-              <h2 className="text-sm text-gray-700 flex gap-1 items-start ">
-                <MdPeopleAlt className="mt-0.5 shrink-0 text-blue-600" />
-                {trip?.userSelection?.traveler}
-              </h2>
-              <h2 className="text-sm text-gray-700 flex gap-1 items-start">
-                <LuCalendarDays className="mt-0.5 shrink-0 text-orange-950" />
-                {trip?.userSelection?.days}
-              </h2>
-            </div>
-          </div>
-        ))}
+        {userTrips.length>0?userTrips
+          .slice()
+          .reverse()
+          .map((trip, idx) => (
+            <MyTripCard trip={trip} idx={idx} />
+          )):
+          [1,2,3,4,5,6,7,8].map(()=>(
+          <div className="h-[250px] w-full bg-slate-300 animate-pulse rounded-xl"></div>
+          ))
+          }
       </div>
     </div>
   );
